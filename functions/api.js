3e0c230862159ba6d94e6846e9de41bf2f382494
Rohit Gadhave <rohit.gadhave@brainwaveconsulting.co.in>
2024-07-09 14:34:52 +0530

    netlify redirects

--- functions/api.js
+++ functions/api.js
@@ -9,12 +9,15 @@ app.use(cors());
 router.get('/', (req, res) => {
   res.send('App is running..');
 });
-
+router.get('/api', (req, res) => {
+    res.send('api App is running..');
+  });
 router.use(express.json());
 
 router.get("/tab", async (req, res) => {
 	res.send('hello oppp');
 })
 
+app.use('/api', router);
 app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app);
