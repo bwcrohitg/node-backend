b88cd7b9ac3d9ea3394181bfa51b1b9aa4f637ea
Rohit Gadhave <rohit.gadhave@brainwaveconsulting.co.in>
2024-07-09 14:38:54 +0530

    tab data added

--- functions/api.js
+++ functions/api.js
@@ -1,23 +1,64 @@
-const express = require('express');
-const serverless = require('serverless-http');
+const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
 /* To troubleshoot CORS error */
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-router.get('/', (req, res) => {
-  res.send('App is running..');
+router.get("/", (req, res) => {
+  res.send("App is running..");
 });
-router.get('/api', (req, res) => {
-    res.send('api App is running..');
-  });
+router.get("/api", (req, res) => {
+  res.send("api App is running..");
+});
 router.use(express.json());
 
 router.get("/tab", async (req, res) => {
-	res.send('hello oppp');
-})
-
-app.use('/api', router);
-app.use('/.netlify/functions/api', router);
+  res.send("hello oppp");
+});
+router.get("/tab-1", (req, res) => {
+  const data = [];
+  for (let index = 0; index < 10000; index++) {
+    const item = {
+      id: index,
+      from: "tab-1",
+      random: Math.floor(Math.random() * 10000),
+      name: "User " + index,
+      createdOn: new Date().getTime(),
+    };
+    data.push(item);
+  }
+  res.send({ data });
+});
+router.get("/tab-2", (req, res) => {
+  const data = [];
+  for (let index = 0; index < 10000; index++) {
+    const item = {
+      id: index,
+      from: "tab-2",
+      random: Math.floor(Math.random() * 10000),
+      name: "User " + index,
+      createdOn: new Date().getTime(),
+    };
+    data.push(item);
+  }
+  res.send({ data });
+});
+router.get("/tab-3", (req, res) => {
+  const data = [];
+  for (let index = 0; index < 10000; index++) {
+    const item = {
+      id: index,
+      from: "tab-3",
+      random: Math.floor(Math.random() * 10000),
+      name: "User " + index,
+      createdOn: new Date().getTime(),
+    };
+    data.push(item);
+  }
+  res.send({ data });
+});
+app.use("/api", router);
+app.use("/.netlify/functions/api", router);
 module.exports.handler = serverless(app);
